5eade2b5ffefd04833a5b8b8e82f86104613037d
Niklas Heim <heim.niklas@gmail.com>
2020-06-18 13:23:02 +0200

    fix test

--- tests/test_esn.py
+++ tests/test_esn.py
@@ -4,15 +4,10 @@ import jax.numpy as jnp
 from esn.esn import sparse_esncell, apply_sparse_esn, generate_state_matrix
 
 def test_sparse_esn():
-    
     esn = sparse_esncell(1,100)
-    inputs = jnp.ones((10,1))
+    inputs = jnp.ones((100,1))
     X  = generate_state_matrix(esn, inputs, 2)
-
-    plt.plot(X[:,1:3])
-    plt.show()
-    raise
-    assert X.shape == (8, 102)
+    assert X.shape == (98, 102)
 
 if __name__ == "__main__":
     test_sparse_esn()
