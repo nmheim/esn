fbb04bea17703053adac4ff6b06f84360f6f98eb
Niklas Heim <niklas.heim@aic.fel.cvut.cz>
2020-08-05 13:48:54 +0200

    clean up

--- esn/optimize.py
+++ esn/optimize.py
@@ -1,7 +1,9 @@
 import jax
 import jax.numpy as jnp
+from esn.imed import imed_matrix
 
-#@jax.jit
+
+#@jax.jit  # TODO: this does not work yet because of dynamic shapes due to 'n'
 def lstsq_stable(H, labels):
     if labels.ndim != 2:
         raise ValueError("Labels must have shape (time, features)")
@@ -17,3 +19,26 @@ def lstsq_stable(H, labels):
 
     wout = jnp.dot(jnp.dot(L, v) / s[:n], uh)
     return wout
+
+
+#@jax.jit  # TODO: this does not work yet because of dynamic shapes due to 'n'
+def imed_lstsq_stable(states, inputs, labels):
+    flat_inputs = inputs.reshape(inputs.shape[0], -1)
+    flat_labels = labels.reshape(inputs.shape[0], -1)
+
+    # prep IMED
+    G = imed_matrix(inputs.shape[1:])
+    (w,V) = jnp.linalg.eigh(G)
+    s = jnp.sqrt(w)
+    G12 = jnp.dot(V, s[:,None]*V.T)
+
+    # transform inputs / labels
+    flat_inputs = jnp.matmul(G12, flat_inputs[:,:,None])[:,:,0]
+    flat_labels = jnp.matmul(G12, flat_labels[:,:,None])[:,:,0]
+
+    # compute Who
+    Who  = lstsq_stable(states, flat_labels)
+    s    = 1/jnp.sqrt(w)
+    iG12 = jnp.dot(V,s[:,None]*V.T)
+    Who  = jnp.dot(iG12, Who)
+    return Who
