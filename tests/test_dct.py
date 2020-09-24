33adf3fd11a8411a0af6bcf579bf286508a62dd3
Niklas Heim <niklas.heim@aic.fel.cvut.cz>
2020-09-24 17:48:00 +0200

    more dct tests

--- tests/test_dct.py
+++ tests/test_dct.py
@@ -1,13 +1,24 @@
 import numpy as np
 import jax.numpy as jnp
-from esn.dct import dct2
 from scipy.fftpack import dctn
+
+from esn.dct import dct2
+from esn.toydata import gauss2d_sequence
 
 def sp_dct2(Fxx, nk1, nk2):
     return dctn(Fxx, norm="ortho")[:nk1,:nk2]
 
-def test_dct():
+def test_random_dct():
     x = np.random.uniform(size=(10,10))
     y = np.array(dct2(jnp.array(x), 3, 3))
     z = sp_dct2(x, 3, 3)
     assert jnp.allclose(y,z)
+
+def test_gauss_dct():
+    x = gauss2d_sequence(size=[30,30])[0]
+    y = np.array(dct2(jnp.array(x), 15, 15))
+    z = sp_dct2(x , 15, 15)
+    assert jnp.allclose(y,z)
+
+if __name__ == "__main__":
+    test_gauss_dct()
